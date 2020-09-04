70c1583db5fc095dbef27c09ee6fbe90a98e61c8
keithmifsud17 <keitmifs@gmail.com>
2020-09-04 22:39:18 +0200

    Builder Files

---
+++ RoundRobin.DependencyFactory/RoundRobinTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoundRobin.DependencyFactory
+{
+    internal class RoundRobinTracker<TService> where TService : class
+    {
+        private readonly RoundRobinList<Type> roundRobin;
+
+        public RoundRobinTracker(IEnumerable<Type> types)
+        {
+            roundRobin = new RoundRobinList<Type>(types);
+        }
+
+        public Type NextType() => roundRobin.Next();
+    }
+}
